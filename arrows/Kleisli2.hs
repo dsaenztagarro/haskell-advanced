fbe2c33f520783c63647c89844c34f109022b03a
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-03-30 08:35:55 +0200

    Arrow of stream functions

--- arrows/Kleisli2.hs
+++ arrows/Kleisli2.hs
@@ -6,3 +6,11 @@ import Control.Monad
 count w = Kleisli readFile >>>
           arr words >>> arr (filter (==w)) >>> arr length >>>
           Kleisli print
+
+-- Stream functions
+
+newtype SF a b = SF {runSF :: [a] -> [b]}
+
+instance Arrow SF where
+    arr f = SF (map f)
+    SF f >>> SF g = SF (f >>> g)
