deddb255c82ea7fc3ff7482e9253a9028904ed8a
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-04-16 10:16:55 +0200

    Example 1 Continuation Monad divided in 2 steps

--- ContinuationMonad.hs
+++ ContinuationMonad.hs
@@ -6,8 +6,8 @@ data Target = Monster
 
 swingAxeBack value = print ("SwingAxeBack " ++ (show value))
 
-isTargetValid :: Target -> Bool
-isTargetValid target = target == Monster
+isTargetValid :: Target -> IO Bool
+isTargetValid target = return $ target == Monster
 
 sayUhOh :: IO ()
 sayUhOh = print "sayUhOh!"
@@ -17,15 +17,38 @@ attack target = if target == Monster
                    then print "Monster attacked!"
                    else print "NPC attacked!"
 
+-- Example 1: ContT as Monad transformer
 
-newtype ContT r m a = ContT { runContT :: (a -> m r) -> m r }
-
-unitAttack :: Target -> ContT () IO Target
-unitAttack target = ContT $ \todo -> do
+unitAttack :: Target -> (Target -> IO ()) -> IO ()
+unitAttack target todo = do
     swingAxeBack 60
-    valid <- return $ isTargetValid target
+    valid <- isTargetValid target
     if valid
     then todo target
     else sayUhOh
 
--- runConT (unitAttack Monster) $ attack
+-- ghci> unitAttack Monster attack
+
+newtype ContT r m a = ContT { runContT :: (a -> m r) -> m r }
+
+unitAttack''' :: Target -> ContT () IO Target
+unitAttack''' target = ContT $ \todo -> do
+    swingAxeBack 60
+    valid <- isTargetValid target
+    if valid
+    then todo target
+    else sayUhOh
+
+-- ghci> runContT (unitAttack Monster) $ attack
+
+-- Example 2: Variable arguments
+
+data Hole = Swing Int | Attack Target
+
+unitAttack2 :: Target -> ContT () IO Hole
+unitAttack2 target = ContT $ \k -> do
+    k (Swing 60)
+    valid <- isTargetValid target
+    if valid
+    then k (Attack target)
+    else sayUhOh
